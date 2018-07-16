7a7e1c5229db9eb4d7a50e08d2d3109c66a9f8b6
Jason Stedman <namdets@gmail.com>
2018-07-16 15:53:07 -0400

    changed filtering to use separate drug and event, added event type-in filter, added session based filter persistence

--- note-browser/src/main/webapp/patientList/patientList.js
+++ note-browser/src/main/webapp/patientList/patientList.js
@@ -5,11 +5,18 @@ define(["text!patientList/patientListContainer.hbs", "common/session", "text!pat
 				$('#app').html(patientListContainer);
 
 				var params = new URLSearchParams(window.location.search);
-
+				
 				var selectedDrugCuis = [];
 				var selectedEventCuis = [];
 				var selectedCandidates = [];
 
+				if(sessionStorage && sessionStorage.selectedCuis){
+					var selections = JSON.parse(sessionStorage.getItem('selectedCuis'));
+					selectedDrugCuis = selections.selectedDrugCuis;
+					selectedEventCuis = selections.selectedEventCuis;
+				}
+
+					
 				var synMapCandidates = function(candidates){
 					var postSynData = {};
 					_.each(candidates, function(candidate){
@@ -31,68 +38,95 @@ define(["text!patientList/patientListContainer.hbs", "common/session", "text!pat
 				}
 
 				var filterPatients = function(){
+					
+					var selectedPairs = [];
+					_.each(selectedDrugCuis, function(drugCui){
+						_.each(selectedEventCuis, function(eventCui){
+							selectedPairs.push({drugCui : drugCui, eventCui : eventCui});
+						});
+					});
+					
 					var drugEnabledPatients = [];
 					var eventEnabledPatients = [];
-					var candidateEnabledPatients = [];
-					if(selectedCandidates.length > 0){
-						_.each(selectedCandidates, function(candidate){
-							var cuis = candidate.split('-');
-							candidateEnabledPatients.push($('#patientList .patient-info-block').has('.btn-candidate.drug-'+cuis[0]+'.event-'+cuis[1]));
-						});
-					}
-					if(selectedDrugCuis.length > 0){
-						_.each(selectedDrugCuis, function(drugCui){
-							drugEnabledPatients.push($('#patientList .patient-info-block').has('.btn-candidate.drug-'+drugCui));
-						});
-					}
-					// Hide all patients if any filters are applied, otherwise show all patients
-					if(drugEnabledPatients.length > 0 || candidateEnabledPatients.length > 0 || eventEnabledPatients.length > 0){
-						$('.patient-info-block').hide();
-						$('.btn-candidate').hide();
-						$('.candidate-validation-indicator').hide();
-					}else{
-						$('.patient-info-block').show();
-						$('.btn-candidate').show();
-						$('.candidate-validation-indicator').show();
-					}
-					// If we have any selected candidates, we show patients based off of the candidate
-					if(selectedCandidates.length > 0){
-						_.each(candidateEnabledPatients, function(patient){
-							patient.show();
-						});
-						_.each(selectedCandidates, function(candidateId){
-							var cuis = candidateId.split('-');
-							$('.btn-candidate.drug-'+cuis[0]+'.event-'+cuis[1]).show();
+					var pairEnabledPatients = [];
+
+					if(selectedPairs.length > 0){
+						_.each(selectedPairs, function(pair){
+							pairEnabledPatients.push($('#patientList .patient-info-block')
+									.has('.btn-candidate.drug-'+pair.drugCui+'.event-'+pair.eventCui));
 						});
 
-					// If we have no selected candidates we show patients based on drug
-					}else{
-						_.each(drugEnabledPatients, function(patient){
+						// Hide all patients if any filters are applied, otherwise show all patients
+							$('.patient-info-block').hide();
+							$('.btn-candidate').hide();
+							$('.candidate-validation-indicator').hide();
+						_.each(pairEnabledPatients, function(patient){
 							patient.show();
-						});						
+						});
+						_.each(selectedPairs, function(pair){
+							$('.btn-candidate.drug-'+pair.drugCui+'.event-'+pair.eventCui).show();
+							$('.candidate-validation-indicator.drug-'+pair.drugCui+'.event-'+pair.eventCui).show();
+						});
+					}else{
+						if(selectedDrugCuis.length > 0){
+							_.each(selectedDrugCuis, function(drugCui){
+								drugEnabledPatients.push($('#patientList .patient-info-block').has('.btn-candidate.drug-'+drugCui));
+							});
+						}
+						if(selectedEventCuis.length > 0){
+							_.each(selectedEventCuis, function(eventCui){
+								eventEnabledPatients.push($('#patientList .patient-info-block').has('.btn-candidate.event-'+eventCui));
+							});
+						}
+						// Hide all patients if any filters are applied, otherwise show all patients
+						if(drugEnabledPatients.length > 0 || eventEnabledPatients.length > 0){
+							$('.patient-info-block').hide();
+							$('.btn-candidate').hide();
+							$('.candidate-validation-indicator').hide();
+						}else{
+							$('.patient-info-block').show();
+							$('.btn-candidate').show();
+							$('.candidate-validation-indicator').show();
+						}
+						_.each(_.union(drugEnabledPatients, eventEnabledPatients), function(patient){
+							patient.show();
+						});
 						_.each(selectedDrugCuis, function(drugCui){
 							$('.btn-candidate.drug-'+drugCui).show();
 							$('.candidate-validation-indicator.drug-'+drugCui).show();
 						});
+						_.each(selectedEventCuis, function(eventCui){
+							$('.btn-candidate.event-'+eventCui).show();
+							$('.candidate-validation-indicator.event-'+eventCui).show();
+						});
 					}
+					
 					if(session.userMode()==="validate"){
 						$('.btn-candidate[data-adjudicated=true]').hide();
 						$('.candidate-validation-indicator[data-adjudicated=true]').hide();
 					}
-					
 				};
 
 				var filterCandidates = function(){
-					if(selectedDrugCuis.length > 0){
+					if(selectedDrugCuis.length > 0 || selectedEventCuis.length > 0){
 						$('.candidate-relation-block').hide();
 						selectedDrugCuis.forEach(function(drugCui){
 							$('.candidate-relation-block')
 							.has('.candidate-selection-checkbox.drug-'+drugCui)
 							.show();
+						});	
+						selectedEventCuis.forEach(function(eventCui){
+							$('.candidate-relation-block')
+							.has('.candidate-selection-checkbox.event-'+eventCui)
+							.show();
 						});			
 					}else{
 						$('.candidate-relation-block').show();
 					}
+					sessionStorage.setItem('selectedCuis', JSON.stringify({
+						selectedDrugCuis : selectedDrugCuis,
+						selectedEventCuis : selectedEventCuis
+					}));
 				};
 
 				$.get("/rest/patientHistory/",
@@ -120,11 +154,14 @@ define(["text!patientList/patientListContainer.hbs", "common/session", "text!pat
 						var target = event.target.dataset.targetUrl;
 						router.navigate(target, {trigger: true});
 					});
+
+					filterPatients();
+					filterCandidates();
 				}.bind(this)
 				);
 				$.get("/rest/cui/",
 						function(data, status, jqXHR){
-					var terms = _.uniq(_.map(_.where(data, {"term_class": 1}), function(drug){
+					var drugTerms = _.uniq(_.map(_.where(data, {"term_class": 1}), function(drug){
 						drug.term = synMapper.nameForDrugCui(drug.cui, drug.term);
 						return drug;
 					}), function(drug){
@@ -132,11 +169,35 @@ define(["text!patientList/patientListContainer.hbs", "common/session", "text!pat
 					});
 					var content = HBS.compile(cuiTemplate)({
 						termClassName : "Drug",
-						terms : terms
+						terms : drugTerms
 					});
 
 					$("#drug-filter").html(content);
 
+					var eventTerms = _.uniq(_.map(_.where(data, {"term_class": 2}), function(drug){
+						drug.term = synMapper.nameForDrugCui(drug.cui, drug.term);
+						return drug;
+					}), function(drug){
+						return drug.cui;
+					});
+					var content = HBS.compile(cuiTemplate)({
+						termClassName : "Event",
+						terms : eventTerms
+					});
+
+					$("#event-filter").html(content);
+
+					$('#event-autocomplete').keyup(function(event){
+						var searchTerm = event.target.value.toLowerCase();
+						_.each($('#event-filter .cui-selection-checkbox'), function(checkbox){
+							if(checkbox.dataset.cuiTerm.toLowerCase().indexOf(searchTerm) === -1 && !checkbox.checked){
+								$(checkbox).parent().hide();
+							}else{
+								$(checkbox).parent().show();
+							}
+						});
+					});
+					
 					var getSelectedCuis = function(wrapperId){
 						return _.map(
 								_.where(
@@ -163,23 +224,33 @@ define(["text!patientList/patientListContainer.hbs", "common/session", "text!pat
 						filterCandidates();
 					}.bind(this));
 
-					$("#candidate-filter .candidate-selection-checkbox").click(function(event){
-						selectedCandidates = getSelectedCandidates("#candidate-filter");
+					$("#event-filter .cui-selection-checkbox").click(function(event){
+						selectedEventCuis = getSelectedCuis("#event-filter");
 						filterPatients();
 						filterCandidates();
 					}.bind(this));
+					
+					$('#drug-filter-clear-btn').click(function(event){
+						$('#drug-filter').find('.cui-selection-checkbox').prop('checked', false);
+						selectedDrugCuis = getSelectedCuis("#drug-filter");
+						filterPatients();
+						filterCandidates();
+					});
+					$('#event-filter-clear-btn').click(function(event){
+						$('#event-filter').find('.cui-selection-checkbox').prop('checked', false);
+						selectedEventCuis = getSelectedCuis("#event-filter");
+						filterPatients();
+						filterCandidates();
+					});
+					
+
+					_.each(_.union(selectedEventCuis, selectedDrugCuis), function(cui){
+						$('#'+cui).prop("checked", true);
+					});
 
 				}.bind(this)
 				);
-				$.get("/rest/cui/candidates/",
-						function(data, status, jqXHR){
-					var postSynData = synMapCandidates(data);
-					var content = HBS.compile(candidateTemplate)
-					(_.sortBy(_.values(postSynData), 'drug'));
-					$("#candidate-filter").html(content);
-
-				}.bind(this)
-				);
+				
 			}
 	};
 	return patientList;
